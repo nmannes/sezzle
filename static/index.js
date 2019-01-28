6c669ead0ee31fdec236cd2544f5f53385939459
nmanes <nmannes@gmail.com>
2019-01-27 21:31:16 -0600

    mostly done. Need to make pages refresh and then deploy

--- static/index.js
+++ static/index.js
@@ -2,37 +2,58 @@ var app = new Vue({
     el: '#helloworld',
     data: {
         recentdata:[
-            "2+2=4"
         ],
 
         keymap:[
-            ['C','0','/','='],
+            ['C','0','/','*'],
             ['1','2','3','+'],
             ['4','5','6','-'],
-            ['7','8','9','*']
+            ['7','8','9','=']
             
         ],
 
-        expression:"000"
+        expression:""
         
     },
     delimiters: ['[[',']]'],
     methods: {
         push(char){
-            this.expression += char
+            if (this.expression.length > 3){
+                lastchar = this.expression[this.expression.length - 1]
+                console.log(lastchar)
+            }
+            if (char == '='){
+                this.postreq(this.expression)
+                this.expression = ''
+            }else if (char == 'C'){
+                this.expression = ''
+            }
+            else {
+                if(isNaN(this.expression[this.expression.length - 1])){
+                    if(!isNaN(char)){
+                        this.expression += char
+                    }
+                    else{
+                        this.expression = this.expression.substring(0, this.expression.length - 1) + char
+                    }
+                }
+                else{
+                    this.expression += char
+                }
+            }
+
         },
         getRecentData(){
-            this.$http.get('http://localhost:5000/').then(response => {
-                this.recentdata = response.data
-            }, response => {
-                console.log('error')
+            this.$http.get("http://localhost:5000/recent_data").then(response => {
+                this.recentdata = response.body
+            }, (response) => {
             });
         },
         
-        manInTheMiddle(){
-            var data = this.displaydevice.ip
+        postreq(data){
             this.$http.post('http://localhost:5000/evaluate', {"expression": data}).then(response => {
-                console.log(response.data)
+                console.log(response)
+                this.recentdata = response.body
             }, (response) => {
                 console.log(response)
             });
